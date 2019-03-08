7e973bcf66af2c420bbcc82eb429149e6af51df5
Rainer <rainer@websimple.be>
2019-03-08 15:38:43 +0100

    imnport fabrique

---
+++ tutotheque_pipelines.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Utilisations de pipelines par Tutothèque
+ *
+ * @plugin     Tutothèque
+ * @copyright  2019
+ * @author     Rainer Müller
+ * @licence    GNU/GPL v3
+ * @package    SPIP\Tutotheque\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
