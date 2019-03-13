8dfa8b2630a5ccdebc31b3fe8c8dffb8f5a5e8c6
Rainer <rainer@websimple.be>
2019-03-13 16:27:27 +0100

    Recherche des tutos, encore problème avec le calcul auto des drop downs

---
+++ formulaires/recherche_tutos.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * chargement des valeurs par defaut des champs du #FORMULAIRE_RECHERCHE_TUTOS
+ *
+ * @return array
+ *   le valeurs chargés.
+ */
+function formulaires_recherche_tutos_charger_dist(){
+	$fonction = charger_fonction('criteres_utilises', 'inc/tutoriel');
+	$criteres = $fonction();
+	$criteres = array_column($criteres, 'nom', 'id_groupe');
+	$valeurs = [
+		'recherche' => _request('recherche'),
+		'_criteres' => $criteres,
+	];
+	$mots = [];
+	foreach($criteres AS $critere) {
+		$valeurs[$critere] = _request($critere);
+		if(_request($critere)) {
+			$mots[] = _request($critere);
+		}
+	}
+	$valeurs['mots'] = $mots;
+	return $valeurs;
+}
+
+
