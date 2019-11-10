569baa663b2d177f1476343b61924b044e0db1ca
Rainer Müller <rainer@websimple.be>
2019-11-10 18:25:33 +0100

    oups

---
+++ lang/tutotheque_fr.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'champ_lang' => 'Langue',
+	'choix_tout' => 'Tout',
+
+	// F
+	'fieldset_recherche_avancee_label' => 'Recherche avancée',
+	'fieldset_recherche_simple_label' => 'Recherche simple',
+
+	// T
+	'tutotheque_titre' => 'Tutothèque',
+);
